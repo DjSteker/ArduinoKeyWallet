b0877ff3bfac17d9fb78aae81bb0c9f42c0ab674
DjSteker <37488993+DjSteker@users.noreply.github.com>
2024-08-07 23:07:23 +0200

    Update ArduinoKeyWallet.ino

--- ArduinoKeyWallet.ino
+++ ArduinoKeyWallet.ino
@@ -45,9 +45,9 @@ void setup() {
   display.clearDisplay();
 
   // Inicializar registros si la EEPROM está vacía
-  if (EEPROM.read(0) == 255) { // 255 indica EEPROM sin inicializar
-    writeRecordsToEEPROM();
-  }
+  //if (EEPROM.read(0) == 255) { // 255 indica EEPROM sin inicializar
+  //  writeRecordsToEEPROM();
+  //}
 
   // Cargar registros desde EEPROM
   readRecordsFromEEPROM();
